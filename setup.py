9e71463271adff5707f03285e95f9b949c4455a5
Victor Turrisi <vt.turrisi@gmail.com>
2018-12-18 15:26:51 -0200

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from numpy.distutils.core import setup
+from setuptools import setup
 
+with open('requirements.txt') as f:
+      requirements = [p.strip() for p in f.readlines()]
 
 setup(name='auto_plotter',
       version='0.1.0',
@@ -8,6 +10,6 @@ setup(name='auto_plotter',
       author='Victor Turrisi',
       license='MIT',
       packages=['auto_plotter'],
-      install_requires=[p.strip() for p in open('requirements.txt').readlines()],
+      install_requires=requirements,
       include_package_data=True,
       zip_safe=False)
