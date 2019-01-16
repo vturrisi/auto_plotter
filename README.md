921ff27fba2878bcfb1f97d9aa15b1e7b9b7776e
Victor Turrisi <vt.turrisi@gmail.com>
2019-01-16 14:55:26 -0200

    added examples

--- README.md
+++ README.md
@@ -9,8 +9,30 @@ Simple auto plotting library to provide *cool looking* graphs
 ## To install:
   - pip install . --user
 
+## Examples
+<p align="center">
+  <img width="900" height="250" src="imgs/test_boxplot1.png">
+</p>
+
+<p align="center">
+  <img width="900" height="250" src="imgs/test_boxplot2.png">
+</p>
+
+<p align="center">
+  <img width="900" height="250" src="imgs/test_boxplot3.png">
+</p>
+
+<p align="center">
+  <img width="900" height="250" src="imgs/test_boxplot4.png">
+</p>
+
+<p align="center">
+  <img width="900" height="750" src="imgs/test_boxplot5.png">
+</p>
+
 ## TODO:
   - Line plot
   - Barplot
   - Spider plot
   - And other plots
+
--- test/test_boxplot.py
+++ test/test_boxplot.py
@@ -2,23 +2,17 @@ from auto_plotter.boxplot import create_boxplot
 from auto_plotter.utils import DataLoader
 
 xlabels = ['dataset1', 'dataset2', 'dataset3', 'dataset4', 'dataset5']
-ylabels = ['accuracy']
-boxlabels = ['algorithm1', 'algorithm2', 'algorithm3', 'algorithm4', 'algorithm5',
-             'algorithm6', 'algorithm7', 'algorithm8', 'algorithm9', 'algorithm10']
+ylabels = ['metric1', 'metric2', 'metric3']
+boxlabels = ['algorithm1', 'algorithm2', 'algorithm3', 'algorithm4', 'algorithm5']
 
 files = ['test_data/alg1.csv',
          'test_data/alg2.csv',
          'test_data/alg3.csv',
          'test_data/alg4.csv',
-         'test_data/alg4.csv',
-         'test_data/alg1.csv',
-         'test_data/alg2.csv',
-         'test_data/alg3.csv',
-         'test_data/alg4.csv',
-         'test_data/alg4.csv']
+         'test_data/alg4.csv',]
 
 dl = DataLoader(files, alg_names=boxlabels)
 
-fig = create_boxplot(dl, xlabels, ylabels, title=None)
-fig.savefig('resulting_plots/test_boxplot.pdf')
+fig = create_boxplot([dl] * 3, xlabels, ylabels, title=None, color_map='cool')
+fig.savefig('plots/test_boxplot5.png')
 
